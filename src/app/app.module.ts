97e872330b292dca78c679db26a45afd74771a4a
rafael persch <rafaelpersch@hotmail.com>
2021-03-17 22:40:28 -0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { AnnotationComponent } from './annotation/annotation.component';
+import { AnnotationsComponent } from './annotations/annotations.component';
+import { CategoryComponent } from './category/category.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { WordComponent } from './word/word.component';
+import { WordsComponent } from './words/words.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnnotationComponent,
+    AnnotationsComponent,
+    CategoryComponent,
+    CategoriesComponent,
+    WordComponent,
+    WordsComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
