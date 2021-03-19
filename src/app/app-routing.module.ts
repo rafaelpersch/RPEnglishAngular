bc2deaf4ddf0968d3679d4971ba7ccaf5f96f4df
rafael persch <rafaelpersch@hotmail.com>
2021-03-18 23:15:27 -0300

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnnotationsComponent } from './annotations/annotations.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [  {
-  path: '',
-  component: AppComponent,
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full'
   //canActivate: [AuthService],
-}, {
-  path: 'login',
-  component: AppComponent,
-}, {
-  path: 'annotations',
-  component: AnnotationsComponent,
-}];
+}, { path: 'login', component: LoginComponent }, 
+{ path: 'annotations', component: AnnotationsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CategoriesComponent } from './categories/categories.component';
 import { WordComponent } from './word/word.component';
 import { WordsComponent } from './words/words.component';
 import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { LayoutComponent } from './layout/layout.component';
     CategoriesComponent,
     WordComponent,
     WordsComponent,
-    LayoutComponent
+    LayoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
