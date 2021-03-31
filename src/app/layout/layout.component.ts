6f0bfabd6f3c9bd88fcd5299548dbd8a734f61a6
rafael persch <rafaelpersch@hotmail.com>
2021-03-31 13:23:50 -0300

    logout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { BaseComponent } from '../base/base.component';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-layout',
@@ -9,11 +10,15 @@ import { BaseComponent } from '../base/base.component';
 })
 export class LayoutComponent extends BaseComponent implements OnInit {
 
-  constructor(toastr: ToastrService) { 
+  constructor(private authService:AuthService, toastr: ToastrService) { 
     super(toastr); 
   }
 
   ngOnInit(): void {
+  }
+
+  logout(): void{
+    this.authService.signOut();
   }
 
 }
